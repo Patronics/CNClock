0d26e1c5a609d117f49c45b756d6d50d5c06e3cd
Patrick Leiser <patrick27leiser@yahoo.com>
2021-04-26 15:20:32 -0700

    add rehome functions

--- gCodeFiles/gCodeScript2/gCodeScript2.ino
+++ gCodeFiles/gCodeScript2/gCodeScript2.ino
@@ -9,14 +9,40 @@ void segmentF(int startX, int startY, int scale);
 void markerUp();
 void markerDown();
 void toPrintStart(int startX, int startY);
+int reHome();
+int reHome(bool x, bool y);
 
+void setup() {
+  Serial.begin(250000);
 
-void setup(){
+  // Serial1.begin(unsigned long baud, uint32_t config, int8_t rxPin, int8_t txPin, bool invert)
+  // Serial2.begin(unsigned long baud, uint32_t config, int8_t rxPin, int8_t txPin, bool invert)
+  // The txPin & rxPin can set to any output pin
   
+  Serial1.begin(250000, SERIAL_8N1, 36, 32);
+  //Serial2.begin(250000, SERIAL_8N1, 36, 37);
 }
 
-void loop(){
-  printf("; actual stuff");
+void loop() {
+
+  if(Serial.available()) {
+    int ch = Serial.read();
+    if (ch == '~'){
+      reHome();
+    }
+    Serial1.write(ch);
+    //Serial.write(ch);
+  }
+
+  /*if(Serial2.available()) {
+    int ch = Serial2.read();
+    Serial2.write(ch);
+  }*/
+  
+  if(Serial1.available()) {
+    int ch = Serial1.read();
+    Serial.write(ch);
+  }
 }
 
 
@@ -49,10 +75,26 @@ void markerDown(){
   printf(";markerDown");
 }
 
+int reHome(bool x, bool y){
+  char xChar = ' ';
+  if (x){xChar='X';}
+  char yChar = ' ';
+  if (y){yChar='Y';}
+  char nextCommand[COMMAND_LENGTH];
+  snprintf(nextCommand,COMMAND_LENGTH, "G28 %c%c",xChar,yChar);
+  return sendCommand(nextCommand);
+  
+}
+
+int reHome(){
+  return reHome(true, true);
+  
+}
+
 int goToXY(double x, double y){
   char nextCommand[COMMAND_LENGTH];
   snprintf(nextCommand,COMMAND_LENGTH, "G1 X%fY%f",x,y);
-  sendCommand(nextCommand);
+  return sendCommand(nextCommand);
 }
 
 /*void toPrintStart(int startX, int startY){
